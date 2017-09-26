ef5bc7e42240763dc6889ba24d1aebef3e6e7398
Yu-Chih Tung <yctung@umich.edu>
2017-09-26 16:00:08 -0400

    remove old structure, ready to update to my new code of wrapper JNI

--- app/src/main/jni/Application.mk
+++
@@ -1,5 +0,0 @@
-#APP_STL := stlport_static
-APP_STL := gnustl_static
-APP_CPPFLAGS := -frtti -fexceptions
-#APP_ABI := armeabi
-#APP_PLATFORM := android-8
--- app/src/main/obj/local/armeabi/objs/jnilibsvm/libsvm/svm-scale.o.d
+++
@@ -1,2 +0,0 @@
-/Users/eddyxd/Downloads/AndroidLibSvm/app/src/main/obj/local/armeabi/objs/jnilibsvm/libsvm/svm-scale.o: \
- /Users/eddyxd/Downloads/AndroidLibSvm/app/src/main/jni/libsvm/svm-scale.cpp
